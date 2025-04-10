74771ef5767439a970d9406537be96dfb5e303c1
mokjok <murtaza19aug@gmail.com>
2025-04-10 22:03:25 +0400

    Coding
    Coding is cool.

---
+++ Exercise1.py
@@ -0,0 +1,8 @@
+# 3 Variables
+word1="coding"
+word2= "is"
+word3="cool"
+
+#connect all the words together
+print(word1 + " " + word2 + " " + word3)
+
---
+++ Exercise2.py
@@ -0,0 +1,8 @@
+#integer one
+sun=8
+#integer two
+flower=10
+#sum of two integer
+name= sun + flower
+#result
+print(name)
